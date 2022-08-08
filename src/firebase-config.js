3aca452b2f4a44c1e916b29663ad243e93c991ef
Charles <charlesli41@gmail.com>
2022-08-08 16:24:33 -0400

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCW0SoZGPAHFrqU18OJE9M5uijU1Ay8NNg",
+    authDomain: "dungeons-and-dragons-7b93e.firebaseapp.com",
+    projectId: "dungeons-and-dragons-7b93e",
+    storageBucket: "dungeons-and-dragons-7b93e.appspot.com",
+    messagingSenderId: "931805715642",
+    appId: "1:931805715642:web:b06e467274c1607e95f683",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
